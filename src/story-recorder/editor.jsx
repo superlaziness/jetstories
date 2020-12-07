9ede0d1006ddcaa8f35d09c3e0377eed3dc720ae
superlaziness <stanislav.shemyakin@jetbrains.com>
2020-12-07 19:53:37 +0100

    add mute button to editor

--- src/story-recorder/editor.jsx
+++ src/story-recorder/editor.jsx
@@ -1,8 +1,8 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Button from '@webteam/button';
-import { UploadIcon, TrashIcon } from '@webteam/icons';
+import { UploadIcon, TrashIcon, VolumeIcon, MuteIcon } from '@webteam/icons';
 
 import { useBreakpoint } from '@webteam/breakpoints';
 
@@ -13,12 +13,25 @@ import useEditor from './editor-hook';
 const Editor = ({ onEdit, videoData, onClickCancel, onClickUpload }) => {
   const playerRef = useRef();
   const bp = useBreakpoint();
+  const [muted, setMuted] = useState(true);
   const { trim } = useEditor({ onEdit, blob: videoData, playerRef });
+  console.log('set', setMuted);
+  const handleMute = () => {
+    playerRef.current.muted = !playerRef.current.muted;
+    setMuted(playerRef.current.muted);
+  };
   return (
     <div>
       <div className={bp('player', { sm: 'player__sm' })}>
         {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-        <video ref={playerRef} />
+        <video ref={playerRef} muted playsInline />
+        <Button
+          icon={muted ? <MuteIcon size="s" /> : <VolumeIcon size="s" />}
+          size="s"
+          mode="contrast"
+          className="mute-button"
+          onClick={handleMute}
+        />
       </div>
       <Trimmer onChange={trim} />
       <Button mode="contrast" icon={<UploadIcon />} onClick={onClickUpload}>
